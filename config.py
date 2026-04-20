5c348f48d118e1f427d92e4cd28f6bce35364c32
Javaluc <luciovaldivia37@gmail.com>
2026-04-19 19:13:54 -0700

    Add config.py with game configuration settings

---
+++ config.py
@@ -0,0 +1,17 @@
+# Game Configuration Settings
+
+# Screen Dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Gameplay Constants
+FPS = 60
+SOUND_VOLUME = 0.5
+MAX_PLAYERS = 4
